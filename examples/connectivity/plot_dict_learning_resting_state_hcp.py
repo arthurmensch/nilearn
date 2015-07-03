0ac23a2f2172d34dd86d0020a0d9631a2d159752
Arthur Mensch <arthur.mensch@m4x.org>
2015-07-03 11:21:05 +0200

    wip

--- examples/connectivity/plot_dict_learning_resting_state_hcp.py
+++ examples/connectivity/plot_dict_learning_resting_state_hcp.py
@@ -65,7 +65,7 @@ dict_learning = DictLearning(mask="/home/parietal/amensch/HCP/mask_img.nii.gz", 
                              threshold=n_components, verbose=10, random_state=0,
                              n_jobs=2, n_init=2, l1_ratio=0.4, alpha=3.7, n_iter=1000)
 
-dict_learning.incremental_fit(func_filenames[0:1])
+dict_learning.incremental_fit(func_filenames[0:3])
 # Retrieve the independent components in brain space
 components_img = dict_learning.masker_.inverse_transform(dict_learning.components_)
 # components_img is a Nifti Image object, and can be saved to a file with
