21ec2f7321542195b134d57a4c5ed0a64d07b4ce
Arthur Mensch <arthur.mensch@m4x.org>
2015-07-24 14:13:39 +0200

    !squash 2.6 fix

--- nilearn/input_data/masker_validation.py
+++ nilearn/input_data/masker_validation.py
@@ -57,8 +57,8 @@ def check_embedded_nifti_masker(estimator, multi_subject=True):
             conflict_string += "Parameter {!r} :\n" \
                                "    Masker parameter {!r}" \
                                " - overriding estimator parameter {!r}\n" \
-                .format(param_key, str(new_masker_params[param_key]),
-                        str(estimator_params[param_key]))
+                .format(param_key, new_masker_params[param_key],
+                        estimator_params[param_key])
 
     if conflict_string != "":
         warn_str = "Overriding provided-default estimator parameters with"\
