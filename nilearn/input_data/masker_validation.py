358acf3ddb3b5d2b5c734f06373f55948159a7bf
Arthur Mensch <arthur.mensch@m4x.org>
2015-08-17 11:30:11 +0200

    Removed str() in warning message

--- nilearn/input_data/masker_validation.py
+++ nilearn/input_data/masker_validation.py
@@ -60,9 +60,9 @@ def check_embedded_nifti_masker(estimator, multi_subject=True):
             conflict_string += ("Parameter {0:s} :\n"
                                 "    Masker parameter {1:s}"
                                 " - overriding estimator parameter {2:s}\n"
-                                ).format(str(param_key),
-                                         str(new_masker_params[param_key]),
-                                         str(estimator_params[param_key]))
+                                ).format(param_key,
+                                         new_masker_params[param_key],
+                                         estimator_params[param_key])
 
     if conflict_string != "":
         warn_str = ("Overriding provided-default estimator parameters with"
