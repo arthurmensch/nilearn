c39856e018e4dea1a1ab50e0f8b858c9d445dcdb
Arthur Mensch <arthur.mensch@m4x.org>
2015-09-10 16:05:28 +0200

    Fix: decomposition module

--- nilearn/decomposition/__init__.py
+++ nilearn/decomposition/__init__.py
@@ -4,5 +4,6 @@ variant of the ICA called Canonical ICA.
 """
 from .canica import CanICA
 from .dict_learning import DictLearning
+from .sparse_pca import SparsePCA
 
-__all__ = ['CanICA', 'DictLearning']
+__all__ = ['CanICA', 'DictLearning', 'SparsePCAa']
