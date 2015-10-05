be5d6e4e960fa8eeea057efd4d1e0e7549e33878
Arthur Mensch <arthur.mensch@m4x.org>
2015-10-05 15:27:22 +0200

    downsample HCP

--- examples/dev/downsample_hcp.py
+++ examples/dev/downsample_hcp.py
@@ -3,12 +3,10 @@ from nilearn._utils import check_niimg
 from nilearn.image import index_img
 from nilearn.datasets.func import fetch_hcp_rest
 
-data = fetch_hcp_rest(data_dir='/volatile3', n_subjects=40).func
-reduction_ratio = 0.5
+data = fetch_hcp_rest(data_dir=os.path.expanduser('~/data'), n_subjects=40).func
 
-for i, data in enumerate(data[::4]):
-    if i > 36:
-        img = check_niimg(data)
-        n_samples = img.get_shape()[3]
-        img = index_img(img, slice(0, n_samples, 3))
-        img.to_filename(os.path.expanduser('~/HCP_reduced/record_%i.nii.gz' % i))
+for i, data in enumerate(data[::2]):
+    img = check_niimg(data)
+    n_samples = img.get_shape()[3]
+    img = index_img(img, slice(0, n_samples, 3))
+    img.to_filename(os.path.expanduser('~/data/HCP_reduced/record_%i.nii.gz' % i))
