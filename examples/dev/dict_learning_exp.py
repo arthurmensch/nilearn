187d1bd0ebeeb60027b74e8738a13c873f32577e
Arthur Mensch <arthur.mensch@m4x.org>
2015-10-06 10:43:47 +0200

    downsample HCP

--- examples/dev/dict_learning_exp.py
+++ examples/dev/dict_learning_exp.py
@@ -528,6 +528,6 @@ experiment = Experiment('adhd',
 #
 # # output_dir = run(estimators, experiment)
 # analyse(expanduser('~/output/2015-10-05_17-18-18'), n_jobs=32)
-analyse_incr(expanduser('~/drago_output/2015-10-05_17-18-18'), n_jobs=32, n_run_var=3)
+analyse_incr(expanduser('~/output/2015-10-05_17-18-18'), n_jobs=32, n_run_var=3)
 # plot_full(expanduser('~/drago_exp'))
 # plot_incr(expanduser('~/output/2015-10-05_17-18-18'))
