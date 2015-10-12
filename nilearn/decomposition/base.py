13279b3d38ded077403f2b5f0563bc5051fd2b6c
Arthur Mensch <arthur.mensch@m4x.org>
2015-10-12 16:09:56 +0200

    C memmap intermediary

--- nilearn/decomposition/base.py
+++ nilearn/decomposition/base.py
@@ -15,7 +15,7 @@ import numpy as np
 from scipy import linalg
 from sklearn.externals.joblib import Memory, Parallel, delayed
 from sklearn.linear_model import LinearRegression
-from sklearn.utils import check_random_state
+from sklearn.utils import check_random_state, check_array
 from sklearn.utils.extmath import randomized_svd, randomized_range_finder
 from sklearn.base import BaseEstimator
 import time
@@ -28,11 +28,8 @@ from .._utils.niimg import _safe_get_data
 
 
 def _close_and_remove(file, filename):
-    try:
-        os.close(file)
-        os.remove(filename)
-    except:
-        pass
+    # os.close(file)
+    os.remove(filename)
 
 
 class mask_and_reduce(object):
@@ -276,6 +273,7 @@ class MaskReducer(BaseEstimator):
         n_voxels = np.sum(_safe_get_data(self.masker.mask_img_))
         n_samples = subject_limits[-1]
 
+        data = None
         if not mock:
             if return_mmap or self.n_jobs > 1:
                 if self.temp_folder is None:
@@ -291,20 +289,23 @@ class MaskReducer(BaseEstimator):
 
                 # We initialize data in memory or on disk
                 if self.filename is None:
-                    self.file_, self.filename_ = mkstemp(dir=self.temp_folder_)
-                    atexit.register(lambda: _close_and_remove(self.filename_))
+                    file_descr_C, filename_C = mkstemp(dir=self.temp_folder_)
+                    file_descr, filename = mkstemp(dir=self.temp_folder_)
+                    atexit.register(lambda: _close_and_remove(file_descr_C,
+                                                              filename_C))
+                    atexit.register(lambda: _close_and_remove(file_descr,
+                                                              filename))
                 else:
+                    filename_C = join(self.temp_folder_, self.filename + '_C')
+                    file_descr_C = open(filename_C, 'w+').fileno()
                     self.filename_ = join(self.temp_folder_, self.filename)
                     self.file_ = open(self.filename_, 'w+').fileno()
-                data = np.memmap(self.filename_, dtype='float64',
-                                 order='F', mode='w+',
+                data = np.memmap(filename_C, dtype='float64',
+                                 order='C', mode='w+',
                                  shape=(n_samples, n_voxels))
             else:
                 data = np.empty((n_samples, n_voxels), order='F',
                                 dtype='float64')
-        else:
-            data = None
-
         timings_list = Parallel(n_jobs=self.n_jobs)(
             delayed(_load_single_subject)(
                 self.masker, data, subject_limits, subject_n_samples,
@@ -318,16 +319,25 @@ class MaskReducer(BaseEstimator):
                 i, self.power_iter,
                 self.parity,
             ) for i, (img, confound) in enumerate(zip(imgs, confounds)))
-
-        if not mock and not return_mmap and self.n_jobs > 1:
-            # We used a memory map for multiprocessing, restoring
-            data = np.array(data)
-        self.data_ = data
         self.subject_limits_ = subject_limits
 
         timings = np.concatenate([timings[np.newaxis, :]
                                   for timings in timings_list])
         self.time_ = np.sum(timings, axis=0)
+
+        if return_mmap:
+            data_F = np.memmap(self.filename_, dtype='float64',
+                               order='F', mode='w+',
+                               shape=(n_samples, n_voxels))
+            data_F[:] = data
+            self.data_ = data_F
+            data = None
+            _close_and_remove(file_descr_C, filename_C)
+        elif not mock:
+            self.data_ = check_array(data, order='F', copy=True)
+            data = None
+            if self.n_jobs > 1:
+                _close_and_remove(file_descr_C, filename_C)
         return self
 
 
@@ -397,16 +407,17 @@ def _load_single_subject(masker, data, subject_limits, subject_n_samples,
             U = this_data[indices]
     else:  # compression type = 'none'
         U = this_data
+    timings[0] = time.time() - t0
+    t0 = time.time()
     if not mock:
         data[subject_limits[i]:subject_limits[i + 1], :] = U
-    timings[0] = time.time() - t0
+    timings[1] = time.time() - t0
     return timings
 
 
 class DecompositionEstimator(BaseEstimator, CacheMixin):
     """Base class for decomposition estimator. Handles mask logic, provides
      transform and inverse_transform methods
-    # XXX: Will provide score + sorting method in the future
 
     Parameters
     ==========
