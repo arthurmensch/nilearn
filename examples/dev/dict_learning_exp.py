04c982ebd627c576b90e56965564e433caa3c9c1
Arthur Mensch <arthur.mensch@gmail.com>
2015-10-14 20:22:18 +0200

    Drago

--- examples/dev/dict_learning_exp.py
+++ examples/dev/dict_learning_exp.py
@@ -43,87 +43,90 @@ from nilearn.decomposition.isbi_utils import run, \
 #                         # Stability specific
 #                         n_runs=100)
 
+def adhdh_20():
 # ADHD RSN20 intensive experiment
-# estimators = []
-#
-# estimators.append(DictLearning(alpha=4, batch_size=20,
-#                                compression_type='subsample',
-#                                random_state=0,
-#                                forget_rate=1,
-#                                in_memory=False,
-#                                reduction_ratio=1))
-# for compression_type in ['range_finder', 'subsample']:
-#     for reduction_ratio in np.linspace(0.1, 1, 2):
-#         for alpha in np.linspace(2, 7, 6):
-#             estimators.append(DictLearning(alpha=alpha, batch_size=20,
-#                                            compression_type=compression_type,
-#                                            random_state=0,
-#                                            forget_rate=1,
-#                                            reduction_ratio=reduction_ratio))
-# experiment = Experiment('adhd',
-#                         n_subjects=40,
-#                         smoothing_fwhm=6,
-#                         dict_init='rsn20',
-#                         output_dir=expanduser('~/output'),
-#                         cache_dir=expanduser('~/nilearn_cache'),
-#                         data_dir=expanduser('~/data'),
-#                         n_slices=1,
-#                         n_jobs=24,
-#                         n_epochs=1,
-#                         temp_folder=expanduser('~/temp'),
-#                         n_runs=10)
-#
-# temp_folder = drop_memmmap(estimators, experiment)
-# output_dir = run(estimators, experiment, temp_folder=temp_folder)
-# output_dir = '/volatile/arthur/work/output/2015-10-14_17-17-11'
-# gather_results(output_dir)
-# analyse(output_dir, n_jobs=24)
-# analyse_incr(output_dir, n_jobs=24, n_run_var=1)
-# plot_full(output_dir)
-# plot_incr(output_dir, 0.1)
-# convert_nii_to_pdf(join(output_dir, 'stability'), n_jobs=15)
+    estimators = []
+
+    estimators.append(DictLearning(alpha=4, batch_size=20,
+                                   compression_type='subsample',
+                                   random_state=0,
+                                   forget_rate=1,
+                                   in_memory=False,
+                                   reduction_ratio=1))
+    for compression_type in ['range_finder', 'subsample']:
+        for reduction_ratio in np.linspace(0.1, 1, 2):
+            for alpha in np.linspace(2, 7, 6):
+                estimators.append(DictLearning(alpha=alpha, batch_size=20,
+                                               compression_type=compression_type,
+                                               random_state=0,
+                                               forget_rate=1,
+                                               reduction_ratio=reduction_ratio))
+    experiment = Experiment('adhd',
+                            n_subjects=40,
+                            smoothing_fwhm=6,
+                            dict_init='rsn20',
+                            output_dir=expanduser('~/output'),
+                            cache_dir=expanduser('~/nilearn_cache'),
+                            data_dir=expanduser('~/data'),
+                            n_slices=1,
+                            n_jobs=24,
+                            n_epochs=1,
+                            temp_folder=expanduser('~/temp'),
+                            n_runs=10)
+
+    # temp_folder = drop_memmmap(estimators, experiment)
+    # output_dir = run(estimators, experiment, temp_folder=temp_folder)
+    output_dir = expanduser('~/output/2015-10-14_20-02-30')
+    gather_results(output_dir)
+    analyse(output_dir, n_jobs=24)
+    analyse_incr(output_dir, n_jobs=24, n_run_var=1)
+    plot_full(output_dir)
+    plot_incr(output_dir, 0.1)
+    convert_nii_to_pdf(join(output_dir, 'stability'), n_jobs=15)
 
 
-
-# HCP RSN70 explorative experiment
-estimators = []
-estimators.append(DictLearning(alpha=26, batch_size=20,
-                               compression_type='subsample',
-                               random_state=0,
-                               forget_rate=1,
-                               reduction_ratio=1))
-for compression_type in ['range_finder', 'subsample']:
-    for reduction_ratio in np.linspace(0.1, 1, 10):
-        for alpha in np.linspace(18, 26, 5):
-            estimators.append(DictLearning(alpha=alpha, batch_size=20,
-                                           compression_type=compression_type,
-                                           random_state=0,
-                                           forget_rate=1,
-                                           reduction_ratio=reduction_ratio))
-estimators = []
-for alpha in np.linspace(1, 10, 10):
-    estimators.append(DictLearning(alpha=alpha, batch_size=20,
+def hcp_70():
+    # HCP RSN70 explorative experiment
+    estimators = []
+    estimators.append(DictLearning(alpha=26, batch_size=20,
                                    compression_type='subsample',
                                    random_state=0,
                                    forget_rate=1,
                                    reduction_ratio=1))
-experiment = Experiment('hcp_reduced',
-                        n_subjects=70,
-                        smoothing_fwhm=6,
-                        dict_init='rsn70',
-                        output_dir=expanduser('~/output'),
-                        cache_dir=expanduser('~/nilearn_cache'),
-                        data_dir=expanduser('~/data'),
-                        n_slices=1,
-                        n_jobs=10,
-                        n_epochs=1,
-                        # Out of core dictionary learning specifics
-                        temp_folder=expanduser('~/temp'),
-                        # Stability specific
-                        n_runs=1)
-temp_folder = '/home/parietal/amensch/temp/2015-10-12_17-06-34'
-output_dir = run(estimators, experiment, temp_folder=temp_folder)
-gather_results(output_dir)
-analyse(output_dir, n_jobs=15)
-analyse_incr(output_dir, n_jobs=15,
-             n_run_var=1)
+    for compression_type in ['range_finder', 'subsample']:
+        for reduction_ratio in np.linspace(0.1, 1, 10):
+            for alpha in np.linspace(18, 26, 5):
+                estimators.append(DictLearning(alpha=alpha, batch_size=20,
+                                               compression_type=compression_type,
+                                               random_state=0,
+                                               forget_rate=1,
+                                               reduction_ratio=reduction_ratio))
+    estimators = []
+    for alpha in np.linspace(1, 10, 10):
+        estimators.append(DictLearning(alpha=alpha, batch_size=20,
+                                       compression_type='subsample',
+                                       random_state=0,
+                                       forget_rate=1,
+                                       reduction_ratio=1))
+    experiment = Experiment('hcp_reduced',
+                            n_subjects=70,
+                            smoothing_fwhm=6,
+                            dict_init='rsn70',
+                            output_dir=expanduser('~/output'),
+                            cache_dir=expanduser('~/nilearn_cache'),
+                            data_dir=expanduser('~/data'),
+                            n_slices=1,
+                            n_jobs=10,
+                            n_epochs=1,
+                            # Out of core dictionary learning specifics
+                            temp_folder=expanduser('~/temp'),
+                            # Stability specific
+                            n_runs=1)
+    temp_folder = '/home/parietal/amensch/temp/2015-10-12_17-06-34'
+    output_dir = run(estimators, experiment, temp_folder=temp_folder)
+    gather_results(output_dir)
+    analyse(output_dir, n_jobs=10)
+    analyse_incr(output_dir, n_jobs=10,
+                 n_run_var=1)
+
+adhdh_20()
