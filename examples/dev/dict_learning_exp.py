3650eb41abcc49100b868daa882d307376aafeaf
Arthur Mensch <arthur.mensch@gmail.com>
2015-10-22 17:37:39 +0200

    Drago

--- examples/dev/dict_learning_exp.py
+++ examples/dev/dict_learning_exp.py
@@ -134,14 +134,14 @@ def hcp_70():
                             # Stability specific
                             n_runs=10)
     # temp_folder = '/home/parietal/amensch/temp/2015-10-12_17-06-34'
-    temp_folder = '/home/parietal/amensch/temp/2015-10-20_08-19-37'
+    # temp_folder = '/home/parietal/amensch/temp/2015-10-20_08-19-37'
     # temp_folder = drop_memmmap(estimators, experiment)
-    output_dir = run(estimators, experiment, temp_folder=temp_folder)
-    # output_dir = expanduser('~/output/2015-10-14_23-46-52')
+    # output_dir = run(estimators, experiment, temp_folder=temp_folder)
+    output_dir = expanduser('~/output/2015-10-14_23-46-52')
     # gather_results(output_dir)
-    # analyse(experiment, output_dir, n_jobs=20, limit=9)
-    # analyse_num_exp(experiment, output_dir, n_jobs=20, limit=9,
-    #                 n_run_var=3)
+    analyse(experiment, output_dir, n_jobs=20, limit=9)
+    analyse_num_exp(experiment, output_dir, n_jobs=20, limit=9,
+                    n_run_var=3)
     # plot_full(output_dir, n_exp=2)
     # plot_num_exp(output_dir, reduction_ratio_list=[0.1, 0.2], n_exp=2)
     # plot_full(output_dir)
