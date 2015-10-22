29c850d9aba0da388ef7b305e208f9e46636e972
Arthur Mensch <arthur.mensch@gmail.com>
2015-10-22 20:03:53 +0200

    Drago

--- examples/dev/dict_learning_exp.py
+++ examples/dev/dict_learning_exp.py
@@ -80,7 +80,7 @@ def adhd_20():
     output_dir = run(estimators, experiment)
     # output_dir = expanduser('~/output/2015-10-14_21-02-57')
     # analyse_median_maps(output_dir)
-    gather_results(output_dir=output_dir)
+    # gather_results(output_dir=output_dir)
     # analyse(output_dir, n_jobs=28, limit=12)
     # analyse_num_exp(output_dir, n_jobs=20, n_run_var=4)
     # analyse_median_maps(output_dir)
@@ -139,8 +139,8 @@ def hcp_70():
     # output_dir = run(estimators, experiment, temp_folder=temp_folder)
     output_dir = expanduser('~/output/2015-10-14_23-46-52')
     # gather_results(output_dir)
-    analyse(experiment, output_dir, n_jobs=10, limit=9)
-    analyse_num_exp(experiment, output_dir, n_jobs=10, limit=9,
+    analyse(experiment, output_dir, n_jobs=32, limit=9)
+    analyse_num_exp(experiment, output_dir, n_jobs=32, limit=9,
                     n_run_var=3)
     # plot_full(output_dir, n_exp=2)
     # plot_num_exp(output_dir, reduction_ratio_list=[0.1, 0.2], n_exp=2)
