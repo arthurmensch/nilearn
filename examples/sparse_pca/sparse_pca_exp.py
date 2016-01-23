17f14b44f20f7f1267bef3cf96da8fb0f76d4d58
Arthur Mensch <arthur.mensch@m4x.org>
2016-01-23 16:15:39 +0100

    Tweaked experimento

--- examples/sparse_pca/sparse_pca_exp.py
+++ examples/sparse_pca/sparse_pca_exp.py
@@ -1,12 +1,12 @@
-import warnings
 from os.path import expanduser
 
-from nilearn._utils.sparse_pca_utils import Experiment, gather_results, \
-    display_explained_variance, convert_nii_to_pdf
+import numpy as np
+
+from nilearn._utils.sparse_pca_utils import Experiment, \
+    display_explained_variance_cluster, display_explained_variance_time, \
+    display_density_time
 from nilearn._utils.sparse_pca_utils import run
 from nilearn.decomposition import SparsePCA
-
-import numpy as np
 
 
 def adhd_20(n_jobs=1):
@@ -15,11 +15,11 @@ def adhd_20(n_jobs=1):
                             n_epochs=2,
                             warmup=False,
                             feature_ratio=feature_ratio)
-                  for feature_ratio in np.linspace(1, 10, 3)
+                  for feature_ratio in np.linspace(1, 10, 4)
                   for alpha in np.logspace(-4, -1, 4)]
     estimators = estimators
-    experiment = Experiment('adhd',
-                            n_subjects=40,
+    experiment = Experiment('hcp',
+                            n_subjects=100,
                             smoothing_fwhm=4,
                             dict_init='rsn70',
                             output_dir=expanduser('~/output/sparse_pca'),
@@ -29,13 +29,17 @@ def adhd_20(n_jobs=1):
                             n_jobs=n_jobs,
                             parallel_exp=True,
                             n_runs=1)
-    # output_dir = run(estimators, experiment)
-    gather_results(output_dir=expanduser('~/share/output/'
-                                         'spca_cluster/2016-01-11_15-32-09/'
-                                         'results/'))
+    output_dir = run(estimators, experiment)
+    # gather_results(output_dir=expanduser('~/share/output/'
+    #                                      'spca_cluster/2016-01-11_15-32-09/'
+    #                                      'results/'))
     return output_dir
 
 
 if __name__ == '__main__':
-    warnings.filterwarnings('ignore', category=DeprecationWarning)
-    adhd_20(n_jobs=10)
+    # warnings.filterwarnings('ignore', category=DeprecationWarning)
+    # adhd_20(n_jobs=32)
+    # density('/storage/workspace/amensch/output/sparse_pca/2016-01-19_23-26-40')
+    display_explained_variance_cluster('/home/arthur/drago/output/sparse_pca/2016-01-19_23-26-40')
+    display_explained_variance_time('/home/arthur/drago/output/sparse_pca/2016-01-19_23-26-40')
+    display_density_time('/home/arthur/drago/output/sparse_pca/2016-01-19_23-26-40')
